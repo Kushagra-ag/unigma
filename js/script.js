8b8db76d16a1ea272abffb06926e56ea06cf203f
ukulsh <utkarshkulshrestha98@gmail.com>
2020-08-18 19:32:55 +0530

    mujhe ni aa raha samaj me

--- js/script.js
+++ js/script.js
@@ -23,6 +23,23 @@ function init() {
 	setTimeout(headerTransition, 3000);
 }
 
+$('.gallery ul li a').click(function() {
+	var itemID = $(this).attr('href');
+	$('.gallery ul').addClass('item_open');
+	$(itemID).addClass('item_open');
+	return false;
+});
+$('.close').click(function() {
+	$('.port, .gallery ul').removeClass('item_open');
+	return false;
+});
+
+$(".gallery ul li a").click(function() {
+	$('html, body').animate({
+		scrollTop: parseInt($("#top").offset().top)
+	}, 400);
+});
+
 function headerTransition(e) {
 
 	const el = (e && e.currentTarget) || document.querySelector('.header');
