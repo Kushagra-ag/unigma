4873236d91cc621a03b0957384fd170ef549b743
kushagra-ag <kushag44@yahoo.com>
2020-08-31 23:25:09 +0530

    Changes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 const headerText = ['design', 'innovation', 'brand'];
 
-$(window).on("load", function() {
 
-	// document.querySelector('.header__right__img').addEventListener('load', function(){console.log('loaded'); setTimeout(init, 6000)})
+window.addEventListener("load", function() {
 
-	setTimeout(init, 6000)
+	console.log('loaded');
+
+	let img = document.querySelector('.imgRight');
+	console.log(img)
+
+	img.addEventListener('load', function(){console.log('loadedright')})
+
+	// setTimeout(init, 6000)
 	setTimeout(headerTransition, 9000);
 
 
-	});
+});
 
 function init() {
 
 	const preloader = document.querySelector('.preloader');
-	// console.log(preloader)
 
 	preloader.style.opacity = 0;
 	document.querySelector('.parallax').style.display = "block";
@@ -84,35 +89,11 @@ function headerTransition(e) {
 				headerRight.removeAttribute('style');
 				text.removeAttribute('style');
 				el.addEventListener('click', headerTransition);
-				// -----------------------------------
+
 			}, 1800)
 
 		}, 1800)
 
 	}, 1800)
-
-
 	
 }
-
-
-
-
-
-
-
-
-
-// headerLeft.style.backgroundPosition = "0 35%";
-// 	headerRight.style.backgroundPosition = "0 64.6%";
-
-
-// 	setTimeout(function() {
-
-// 		headerLeft.style.backgroundPosition = "0 60.2%";
-// 		headerRight.style.backgroundPosition = "0 30.6%";
-
-// 		setTimeout(function() {
-
-// 			headerLeft.style.backgroundPosition = "0 96.8%";
-// 			headerRight.style.backgroundPosition = "0 8%";
